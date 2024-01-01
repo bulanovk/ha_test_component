778f6afa23b0685b91b690768115f9f74e04d902
bulanovk <bulanovk@gmail.com>
2024-01-01 18:47:35 +0400

    Fix Depends

--- custom_components/ectocontrol/config_flow.py
+++ custom_components/ectocontrol/config_flow.py
@@ -1,22 +1,50 @@
-from typing import Dict
+import logging
+from typing import Any, Dict, Optional
 
-from homeassistant import config_entries
-from homeassistant.components.nam.config_flow import AUTH_SCHEMA
-from homeassistant.components.obihai.connectivity import validate_auth
-from homeassistant.const import CONF_ACCESS_TOKEN
-from voluptuous import Optional, Any
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant import config_entries, core
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_NAME, CONF_PATH, CONF_URL
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN
+from .const import CONF_REPOS, DOMAIN
 
-CONF_REPOS = "repo"
+_LOGGER = logging.getLogger(__name__)
+
+AUTH_SCHEMA = vol.Schema(
+    {vol.Required(CONF_ACCESS_TOKEN): cv.string, vol.Optional(CONF_URL): cv.string}
+)
+REPO_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_PATH): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
+        vol.Optional("add_another"): cv.boolean,
+    }
+)
 
 
-class EctocontrolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
-    # The schema version of the entries that it creates
-    # Home Assistant will call your migrate method if the version changes
-    VERSION = 1
-    MINOR_VERSION = 1
+def validate_path(path: str) -> None:
+    """Validates a GitHub repo path.
+
+    Raises a ValueError if the path is invalid.
+    """
+    if len(path.split("/")) != 2:
+        raise ValueError
+
+
+async def validate_auth(access_token: str, hass: core.HomeAssistant) -> None:
+    """Validates a GitHub access token.
+
+    Raises a ValueError if the auth token is invalid.
+    """
+    session = async_get_clientsession(hass)
+
+
+
+class GithubCustomConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Github Custom config flow."""
+
+    data: Optional[Dict[str, Any]]
 
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
         """Invoked when a user initiates a flow via the user interface."""
@@ -35,4 +63,34 @@ class EctocontrolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user", data_schema=AUTH_SCHEMA, errors=errors
+        )
+
+    async def async_step_repo(self, user_input: Optional[Dict[str, Any]] = None):
+        """Second step in config flow to add a repo to watch."""
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            # Validate the path.
+            try:
+                validate_path(user_input[CONF_PATH])
+            except ValueError:
+                errors["base"] = "invalid_path"
+
+            if not errors:
+                # Input is valid, set data.
+                self.data[CONF_REPOS].append(
+                    {
+                        "path": user_input[CONF_PATH],
+                        "name": user_input.get(CONF_NAME, user_input[CONF_PATH]),
+                    }
+                )
+                # If user ticked the box show this form again so they can add an
+                # additional repo.
+                if user_input.get("add_another", False):
+                    return await self.async_step_repo()
+
+                # User is done adding repos, create the config entry.
+                return self.async_create_entry(title="GitHub Custom", data=self.data)
+
+        return self.async_show_form(
+            step_id="repo", data_schema=REPO_SCHEMA, errors=errors
         )
