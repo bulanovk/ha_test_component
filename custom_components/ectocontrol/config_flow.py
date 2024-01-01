e56b7da2553e3e9f97dbc6b9846b642e3cfa969d
bulanovk <bulanovk@gmail.com>
2024-01-01 18:43:02 +0400

    Fix Depends

--- custom_components/ectocontrol/config_flow.py
+++ custom_components/ectocontrol/config_flow.py
@@ -1,7 +1,14 @@
+from typing import Dict
+
 from homeassistant import config_entries
-import voluptuous as vol
+from homeassistant.components.nam.config_flow import AUTH_SCHEMA
+from homeassistant.components.obihai.connectivity import validate_auth
+from homeassistant.const import CONF_ACCESS_TOKEN
+from voluptuous import Optional, Any
 
 from .const import DOMAIN
+
+CONF_REPOS = "repo"
 
 
 class EctocontrolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -10,11 +17,22 @@ class EctocontrolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     # Home Assistant will call your migrate method if the version changes
     VERSION = 1
     MINOR_VERSION = 1
-    async def async_step_user(self, info):
-        if info is not None:
-            pass  # TODO: process info
+
+    async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
+        """Invoked when a user initiates a flow via the user interface."""
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            try:
+                await validate_auth(user_input[CONF_ACCESS_TOKEN], self.hass)
+            except ValueError:
+                errors["base"] = "auth"
+            if not errors:
+                # Input is valid, set data.
+                self.data = user_input
+                self.data[CONF_REPOS] = []
+                # Return the form of the next step.
+                return await self.async_step_repo()
 
         return self.async_show_form(
-            step_id="token", data_schema=vol.Schema({vol.Required("token"): str})
+            step_id="user", data_schema=AUTH_SCHEMA, errors=errors
         )
-
