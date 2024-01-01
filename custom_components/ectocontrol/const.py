f8ff6ebf14907064004000574e00639f7e77cdfa
bulanovk <bulanovk@gmail.com>
2024-01-01 21:54:14 +0400

    Fix Depends

--- custom_components/ectocontrol/const.py
+++ custom_components/ectocontrol/const.py
@@ -44,7 +44,7 @@ If you have any issues with this you need to open an issue here:
 """
 
 
-CONF_SCENE_PATH = "scene_path"
+CONF_SCENE_PATH = "system_id"
 CONF_NUMBER_TOLERANCE = "number_tolerance"
 CONF_RESTORE_STATES_ON_DEACTIVATE = "restore_states_on_deactivate"
 CONF_TRANSITION_TIME = "transition_time"
