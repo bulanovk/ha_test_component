9b12a9160ef4fef1f73b28479c2a0431bc6a0285
bulanovk <bulanovk@gmail.com>
2024-01-01 19:51:19 +0400

    Fix Depends

--- custom_components/ectocontrol/config_flow.py
+++ custom_components/ectocontrol/config_flow.py
@@ -1,50 +1,32 @@
 import logging
 from typing import Any, Dict, Optional
 
-import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from homeassistant import config_entries, core
-from homeassistant.const import CONF_ACCESS_TOKEN
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant import config_entries
+from homeassistant import data_entry_flow
+
 
 from .const import *
 
 _LOGGER = logging.getLogger(__name__)
 
-AUTH_SCHEMA = vol.Schema(
-    {vol.Required(CONF_ACCESS_TOKEN): cv.string}
-)
 
-
-async def validate_auth(access_token: str, hass: core.HomeAssistant) -> None:
-    """Validates a GitHub access token.
-
-    Raises a ValueError if the auth token is invalid.
-    """
-    session = async_get_clientsession(hass)
-
-
+@config_entries.HANDLERS.register(DOMAIN)
 class EctocontrolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Ectocontrol Custom config flow."""
+    VERSION = 1
 
     data: Optional[Dict[str, Any]]
 
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
         """Invoked when a user initiates a flow via the user interface."""
-        errors: Dict[str, str] = {}
         if user_input is not None:
-            try:
-                await validate_auth(user_input[ATTR_PUBLIC_TOKEN], self.hass)
-            except ValueError:
-                errors["base"] = "auth"
-            if not errors:
-                # Input is valid, set data.
-                self.data = user_input
-                # Return the form of the next step.
-                return self.async_create_entry(title="Ectocontrol", data=self.data)
+            self.data = user_input
+            # Return the form of the next step.
+            return self.async_create_entry(title="Ectocontrol", data=self.data)
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({vol.Required(ATTR_SYSTEM_ID): str,vol.Required(ATTR_PUBLIC_TOKEN): str})
+            step_id="init",
+            data_schema=vol.Schema({vol.Required(ATTR_SYSTEM_ID): str, vol.Required(ATTR_PUBLIC_TOKEN): str})
 
         )
