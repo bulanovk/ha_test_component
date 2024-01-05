69f331281c00c1b01d2646822b8aced528e4bc7a
bulanovk <bulanovk@gmail.com>
2024-01-05 13:52:58 +0400

    Fix Depends

--- custom_components/ectocontrol/entity.py
+++ custom_components/ectocontrol/entity.py
@@ -33,6 +33,6 @@ class EctocontrolEntity(CoordinatorEntity):
         """Return the state attributes."""
         return {
             "attribution": ATTRIBUTION,
-            # "id": str(self.coordinator.data.get("id")),
+            "id": str(self.coordinator.data.get("id")),
             "integration": DOMAIN,
         }
