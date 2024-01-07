4d1a47b0780edb78a4d272533e279b222dbee2ef
bulanovk <bulanovk@gmail.com>
2024-01-07 22:23:47 +0400

    Fix Depends

--- int/test_api.py
+++ int/test_api.py
@@ -11,7 +11,7 @@ from custom_components.ectocontrol.core.model import EctoControlAPIDevices
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        api = EctocontrolApiClient("05bd6f08a561b183f1eebc81800ad556da385b9c89f484fb9899f87261d6a67b", session)
+        api = EctocontrolApiClient("<TBD>", session)
         data: EctoControlAPIDevices = await api.async_get_devices()
         await api.async_get_data(data)
 
